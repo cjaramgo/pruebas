0b73b1b9b118ab18ed2c4e02ef0f43cf9557323d
cjaramillo <carlos.jaramillog@gmail.com>
2018-09-13 11:12:47 -0500

    Se cambio HolaMundo y se adicionó una nueva clase

--- ProyectoTest/src/pruebas/HolaMundo.java
+++ ProyectoTest/src/pruebas/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		
 		int A = 1;
-		int B = 2;
+		int B = 3;
 		int resultado;
 		resultado = A + B;
 		System.out.println("El resultado es:" + resultado);
---
+++ ProyectoTest/src/pruebas/NuevaClase.java
@@ -0,0 +1,12 @@
+package pruebas;
+
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Soy Carlos");
+
+	}
+
+}
